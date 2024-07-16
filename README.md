fe3acf2774d4f3b339ef64a69f57988a96e3da16
看见我请叫我去背单词 <2865260231@qq.com>
2024-07-17 02:03:02 +0800

    test

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+# 使用到的插件
+1.  Element-ui
+2.  axios
+3.  vue-router
+4.  vuex
+5.  vue-cli
+6.  video.js+vue3-video-player
---
+++ views/VideoDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <video-player
+      src="/your-path/video.mp4"
+      poster="/your-path/poster.jpg"
+      controls
+      fluid
+      autoplay
+      sources=""
+  />
+
+</template>
+
+<script setup>
+import {VideoPlayer} from "@videojs-player/vue";
+
+</script>
+
+
+
+<style scoped>
+
+</style>
